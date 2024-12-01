c892eac88ac0c7887a22d438ed12c77da31c0d7a
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2024-12-01 16:05:45 -0500

    apply hlint

--- src/MyLib.hs
+++ src/MyLib.hs
@@ -2,11 +2,12 @@
 
 module MyLib (part1, part2) where
 
-import Control.Arrow ((***))
+import Control.Arrow
 import Data.List (sort, transpose)
 
 import Data.Map (Map)
 import Data.Map qualified as M
+import Data.Maybe (fromMaybe)
 
 pToList :: String -> [[Int]]
 pToList = transpose . map (map read . words) . lines
@@ -18,12 +19,12 @@ part1 :: String -> Int
 part1 = sum . map (abs . uncurry (-)) . uncurry zip . (sort *** sort) . listPairToTuple . pToList
 
 frequencies :: [Int] -> Map Int Int
-frequencies = foldr (\a -> M.alter f a) mempty
+frequencies = foldr (M.alter f) mempty
   where
     f = Just . maybe 1 succ
 
 part2 :: String -> Int
-part2 = sum . f . (sort *** frequencies) . listPairToTuple . pToList
+part2 = sum . f . second frequencies . listPairToTuple . pToList
   where
     f :: ([Int], Map Int Int) -> [Int]
-    f (a, b) = map (\i -> i * maybe 0 id (M.lookup i b)) a
+    f (a, b) = map (\i -> i * fromMaybe 0 (M.lookup i b)) a
