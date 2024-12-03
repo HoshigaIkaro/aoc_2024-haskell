fd42ae1c31bf2bb5b6bd6a8bca3553775bec5a08
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2024-12-03 03:05:27 -0500

    refactor to use replace-megaparsec

--- src/Days/D3.hs
+++ src/Days/D3.hs
@@ -4,11 +4,11 @@
 module Days.D3 (run, part1, part2) where
 
 import Control.Monad (void)
-import Data.List (isPrefixOf)
-import Data.Maybe (isJust)
+import Data.Either
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Void (Void)
+import Replace.Megaparsec
 import Text.Megaparsec
 import Text.Megaparsec.Char
 import Text.Megaparsec.Char.Lexer qualified as L
@@ -23,59 +23,41 @@ run = do
 
 pPair :: Parser (Int, Int)
 pPair = do
+    void $ char '('
     a <- L.decimal
     void $ char ','
     b <- L.decimal
+    void $ char ')'
     pure (a, b)
 
 pMulPair :: Parser (Int, Int)
-pMulPair = lookAhead (string "mul" *> parentheses pPair) *> (string "mul" *> parentheses pPair) <* (void (many anyChar) <|> eof)
+pMulPair = string "mul" *> pPair
 
-anyChar :: Parser ()
-anyChar = void asciiChar <|> void (char ' ') <|> void symbolChar
-
-lexeme :: Parser a -> Parser a
-lexeme = L.lexeme (void $ many anyChar)
-
-betweenFiller :: Parser a -> Parser a
-betweenFiller = between (many anyChar) (many anyChar)
-
-parentheses :: Parser a -> Parser a
-parentheses = between (char '(') (char ')')
-
--- pAllPairs :: Parser [(Int, Int)]
-pAllPairs = many (recover (many (anySingleBut 'm') *> (string "mul" *> parentheses pPair)))
-  where
-    recover = withRecovery $ \e -> do
-        registerParseError e
-        _ <- void (many (anySingleBut 'm')) <|> eof
-        pure (0, 0)
-
-findPairs :: String -> [(Int, Int)]
-findPairs "" = []
-findPairs str = case parseMaybe pMulPair (T.pack str) of
-    Nothing -> findPairs (drop 1 str)
-    Just pair -> pair : findPairs (drop 1 str)
+pAllPairs :: Parser [Either Text (Int, Int)]
+pAllPairs = sepCap pMulPair
 
 part1 :: String -> Int
-part1 = sum . map (uncurry (*)) . findPairs
+part1 = sum . map (uncurry (*)) . rights . fromRight [] . runParser pAllPairs "" . T.pack
 
 pDo :: Parser ()
-pDo = void $ string "do()" <* (void (many anyChar) <|> eof)
-
+pDo = void $ string "do()"
 pDont :: Parser ()
-pDont = void $ string "don't()" <* (void (many anyChar) <|> eof)
+pDont = void $ string "don't()"
+
+data PResult = Pair (Int, Int) | Do | Dont deriving (Show)
+
+pAllPairsToggleable :: Parser [Either Text PResult]
+pAllPairsToggleable =
+    sepCap $
+        (Pair <$> pMulPair)
+            <|> (Dont <$ pDont)
+            <|> (Do <$ pDo)
 
 part2 :: String -> Int
-part2 = sum . map (uncurry (*)) . findPairsToggleable True . T.pack
-
-findPairsToggleable :: Bool -> Text -> [(Int, Int)]
-findPairsToggleable _ "" = []
-findPairsToggleable False str
-    | "do()" `T.isPrefixOf` str = findPairsToggleable True (T.drop 4 str)
-    | otherwise = findPairsToggleable False (T.drop 1 str)
-findPairsToggleable True str
-    | "don't()" `T.isPrefixOf` str = findPairsToggleable False (T.drop 7 str)
-    | otherwise = case parseMaybe pMulPair str of
-        Nothing -> findPairsToggleable True (T.drop 1 str)
-        Just pair -> pair : findPairsToggleable True (T.drop 1 str)
+part2 = snd . foldr go (True, 0) . reverse . rights . fromRight [] . runParser pAllPairsToggleable "" . T.pack
+  where
+    go Dont (_, acc) = (False, acc)
+    go Do (_, acc) = (True, acc)
+    go (Pair (a, b)) (enabled, acc)
+        | enabled = (enabled, acc + a * b)
+        | otherwise = (enabled, acc)
