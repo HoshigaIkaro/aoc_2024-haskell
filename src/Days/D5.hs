c415a510713e907f07dbf46d56d05bbbce8873ed
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2024-12-05 02:03:23 -0500

    refactor part 1 to use sortBy

--- src/Days/D5.hs
+++ src/Days/D5.hs
@@ -3,12 +3,12 @@
 
 module Days.D5 (run, part1, part2) where
 
-import Data.List
+import Data.List ( sortBy )
 import Data.Map (Map)
 import Data.Map qualified as M
-import Data.Maybe
 import Data.Text (Text)
 import Data.Text qualified as T
+import Data.Maybe (fromMaybe)
 
 run :: IO ()
 run = do
@@ -16,15 +16,8 @@ run = do
     print $ part1 input
     print $ part2 input
 
-pRules :: Text -> Map Int [Int]
+pRules :: Text -> Map (Int, Int) Ordering
 pRules = foldr processLine M.empty . T.lines
-  where
-    toTuple lst = (head lst, lst !! 1)
-    updateRules (smaller, larger) = M.alter (maybe (Just [smaller]) (Just . (smaller :))) larger
-    processLine = updateRules . toTuple . map (read . T.unpack) . T.split (== '|')
-
-pRulesAlt :: Text -> Map (Int, Int) Ordering
-pRulesAlt = foldr processLine M.empty . T.lines
   where
     toTuple lst = (head lst, lst !! 1)
     updateRules = flip M.insert LT
@@ -33,7 +26,7 @@ pRulesAlt = foldr processLine M.empty . T.lines
 pUpdate :: Text -> [Int]
 pUpdate = map (read . T.unpack) . T.split (== ',')
 
-pInput :: String -> (Map Int [Int], [[Int]])
+pInput :: String -> (Map (Int, Int) Ordering, [[Int]])
 pInput s = (rules, updates)
   where
     t = T.pack s
@@ -41,24 +34,10 @@ pInput s = (rules, updates)
     rules = pRules (head halves)
     updates = map pUpdate (T.lines $ last halves)
 
-pInputAlt :: String -> (Map (Int, Int) Ordering, [[Int]])
-pInputAlt s = (rules, updates)
-  where
-    t = T.pack s
-    halves = T.splitOn "\n\n" t
-    rules = pRulesAlt (head halves)
-    updates = map pUpdate (T.lines $ last halves)
-
-updateIsCorrectAlt :: Map (Int, Int) Ordering -> [Int] -> Bool
-updateIsCorrectAlt mapping lst = all f $ zip lst (drop 1 lst)
+updateIsCorrect :: Map (Int, Int) Ordering -> [Int] -> Bool
+updateIsCorrect mapping lst = all f $ zip lst (drop 1 lst)
   where
     f (a, b) = M.notMember (b, a) mapping
-
-updateIsCorrect :: Map Int [Int] -> [Int] -> Bool
-updateIsCorrect _ [] = True
-updateIsCorrect mapping (x : xs) = noInvalidInRemaining && updateIsCorrect mapping xs
-  where
-    noInvalidInRemaining = all (`notElem` xs) (fromMaybe [] $ M.lookup x mapping)
 
 middleNumber :: [Int] -> Int
 middleNumber lst = lst !! mid
@@ -70,22 +49,12 @@ part1 s = sum . map middleNumber . filter (updateIsCorrect rules) $ updates
   where
     (rules, updates) = pInput s
 
-fixUpdate :: Map Int [Int] -> [Int] -> [Int]
-fixUpdate _ [] = []
-fixUpdate mapping (x : xs)
-    | not (null smallerThanX) = fixUpdate mapping updatedList
-    | otherwise = x : fixUpdate mapping xs
+fixUpdate :: Map (Int, Int) Ordering -> [Int] -> [Int]
+fixUpdate mapping = sortBy sortFunc
   where
-    smallerThanX = filter (`elem` xs) . fromMaybe [] $ M.lookup x mapping
-    greatestIndex = maximum $ mapMaybe (`elemIndex` xs) smallerThanX
-    updatedList = take (greatestIndex + 1) xs <> [x] <> drop (greatestIndex + 1) xs
-
-fixUpdateAlt :: Map (Int, Int) Ordering -> [Int] -> [Int]
-fixUpdateAlt mapping = sortBy sortFunc
-  where
-    sortFunc a b = maybe GT id (M.lookup (a, b) mapping)
+    sortFunc a b = fromMaybe GT (M.lookup (a, b) mapping)
 
 part2 :: String -> Int
-part2 s = sum . map (middleNumber . fixUpdateAlt rules) . filter (not . updateIsCorrectAlt rules) $ updates
+part2 s = sum . map (middleNumber . fixUpdate rules) . filter (not . updateIsCorrect rules) $ updates
   where
-    (rules, updates) = pInputAlt s
+    (rules, updates) = pInput s
