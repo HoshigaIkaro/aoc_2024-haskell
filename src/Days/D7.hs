915687d5216445822ce8f69186567fe5219efe80
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2024-12-07 00:32:12 -0500

    rename isPossible and reorder parameters

--- src/Days/D7.hs
+++ src/Days/D7.hs
@@ -8,7 +8,7 @@ import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Void (Void)
 import Text.Megaparsec
-import Text.Megaparsec.Char (char, eol, newline, space, string)
+import Text.Megaparsec.Char (space, string)
 import Text.Megaparsec.Char.Lexer qualified as L
 
 type Parser = Parsec Void Text
@@ -29,20 +29,17 @@ pLine = do
 pInput :: Text -> [(Int, [Int])]
 pInput = map (fromJust . parseMaybe pLine) . T.lines
 
-operatorsPossible :: Int -> [Int] -> [Int -> Int -> Int] -> Int -> Bool
-operatorsPossible target [] _ current = target == current
-operatorsPossible target (x : xs) ops current =
-    any (\op -> operatorsPossible target xs ops (current `op` x)) ops
+isPossible :: [Int -> Int -> Int] -> Int -> Int -> [Int] -> Bool
+isPossible _ current target [] = target == current
+isPossible ops current target (x : xs) = any (\op -> isPossible ops (current `op` x) target xs) ops
 
 part1 :: String -> Int
-part1 s = sum . map fst . filter (\(n, vals) -> operatorsPossible n vals [(+), (*)] 0) . pInput $ T.pack s
-
-operatorsWithConcatPossible :: Int -> [Int] -> [Int -> Int -> Int] -> Int -> Bool
-operatorsWithConcatPossible target [] _ current = target == current
-operatorsWithConcatPossible target (x : xs) ops current =
-    any (\op -> operatorsWithConcatPossible target xs ops (current `op` x)) ops
+part1 = sum . map fst . filter (uncurry (isPossible operators 0)) . pInput . T.pack
+  where
+    operators = [(+), (*)]
 
 part2 :: String -> Int
-part2 s = sum . map fst . filter (\(n, vals) -> operatorsWithConcatPossible n vals [(+), (*), f] 0) . pInput $ T.pack s
-    where
-        f a b = read (show a <> show b)
+part2 = sum . map fst . filter (uncurry (isPossible operators 0)) . pInput . T.pack
+  where
+    f a b = read (show a <> show b)
+    operators = [(+), (*), f]
