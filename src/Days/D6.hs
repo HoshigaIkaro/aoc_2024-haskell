fb879b9df31f8cb12d342fa4c61c32ca1ba815d5
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2024-12-06 22:56:57 -0500

    cleanup and apply hlint

--- src/Days/D6.hs
+++ src/Days/D6.hs
@@ -3,8 +3,6 @@
 module Days.D6 (run, part1, part2) where
 
 import Control.Arrow
-import Control.DeepSeq
-import Control.Parallel.Strategies
 import Data.Ix (Ix (inRange))
 import Data.List
 import Data.Maybe
@@ -90,22 +88,6 @@ moveGuardUntilLeave b = go direction position S.empty
 part1 :: String -> Int
 part1 = S.size . moveGuardUntilLeave . pInput
 
-isCycle :: Board -> Bool
-isCycle b = go direction position S.empty
-  where
-    width = bWidth b
-    height = bHeight b
-    walls = bWalls b
-    (position, direction) = bGuard b
-    inBounds (row, col) = inRange (0, height - 1) row && inRange (0, width - 1) col
-    go dir point visited
-        | (point, dir) `elem` visited = True
-        | not (inBounds point) = False
-        | newPoint `elem` walls = go (nextDir dir) point (S.insert (point, dir) visited)
-        | otherwise = go dir newPoint (S.insert (point, dir) visited)
-      where
-        newPoint = getMove dir point
-
 nextWall :: Walls -> Point -> Direction -> Maybe Point
 nextWall walls (row, col) dir
     | dir == UP = fmap snd $ unsnoc $ sort (S.toList $ S.filter (\(newRow, newCol) -> newRow < row && newCol == col) walls)
@@ -115,96 +97,21 @@ nextWall walls (row, col) dir
     | otherwise = Nothing
 
 nextPoint :: Walls -> Point -> Direction -> Maybe Point
-nextPoint walls point dir = nextWall walls point dir >>= pure . getOppositeMove dir
+nextPoint walls point dir = getOppositeMove dir <$> nextWall walls point dir
 
-isLoop :: Set Point -> Set (Point, Direction) -> Point -> Direction -> Maybe Bool
-isLoop walls visited point dir = do
+isLoop :: Set (Point, Direction) -> Point -> Direction -> Walls -> Maybe Bool
+isLoop visited point dir walls = do
     let newDir = nextDir dir
     newPoint <- nextPoint walls point dir
     let newState = (newPoint, newDir)
     if newState `S.member` visited
         then pure True
-        else isLoop walls (S.insert newState visited) newPoint newDir
+        else isLoop (S.insert newState visited) newPoint newDir walls
 
-findCycles :: Board -> Set Point
-findCycles b = go direction position
+part2 :: String -> Int
+part2 s = length $ filter (fromMaybe False . isLoop S.empty point dir) newWallSets
   where
-    width = bWidth b
-    height = bHeight b
-    walls = bWalls b
-    (position, direction) = bGuard b
-    inBounds (row, col) = inRange (0, height - 1) row && inRange (0, width - 1) col
-    go dir point
-        | not (inBounds point) = S.empty
-        | newPoint `elem` walls = go (nextDir dir) point
-        | otherwise =
-            if not (inBounds newPoint)
-                then S.empty
-                else case canMakeCycleInFront (S.singleton (newPoint, dir)) newPoint dir of
-                    Nothing -> go dir newPoint
-                    Just True -> S.insert newPoint $ go dir newPoint
-                    Just False -> go dir newPoint
-      where
-        newPoint = getMove dir point
-        wallsUpdated = S.insert newPoint walls
-        canMakeCycleInFront vWall wallPoint cDir = do
-            let newDir = nextDir cDir
-            newWall <- nextWall wallsUpdated (getOppositeMove cDir wallPoint) newDir
-            let newState = (newWall, newDir)
-            if newState `S.member` vWall
-                then pure True
-                else canMakeCycleInFront (S.insert newState vWall) newWall newDir
-
--- canMakeCycleInFront = do
---     let newDir1 = nextDir dir
---     newWall1 <- nextWall wallsUpdated point newDir1
---     let newDir2 = nextDir newDir1
---     newWall2 <- nextWall wallsUpdated (getOppositeMove newDir1 newWall1) newDir2
---     let newDir3 = nextDir newDir2
---     newWall3 <- nextWall wallsUpdated (getOppositeMove newDir2 newWall2) newDir3
---     let newDir4 = nextDir newDir3
---     newWall4 <- nextWall wallsUpdated (getOppositeMove newDir3 newWall3) newDir4
---     pure (newPoint == newWall4)
-
--- isCycleAlt :: Board -> Bool
--- isCycleAlt b = go direction position False S.empty
---   where
---     width = bWidth b
---     height = bHeight b
---     walls = bWalls b
---     (position, direction) = bGuard b
---     inBounds (row, col) = inRange (0, height - 1) row && inRange (0, width - 1) col
---     go dir point previousWasTurn visited
---         | not previousWasTurn && (point, dir) `elem` visited = True
---         | not (inBounds point) = False
---         | newPoint `elem` walls = go (nextDir dir) point True (S.insert (point, dir) visited)
---         | not previousWasTurn && (point, dir) `S.member` visited = True
---         | otherwise = go dir newPoint False (S.insert (point, dir) visited)
---       where
---         newPoint = getMove dir point
-
--- part2 :: String -> Int
--- part2 s = S.size $ findCycles b
-part2 s = length $ filter (\w -> fromMaybe False $ uncurry (isLoop w S.empty) (bGuard b)) newWallSets
-  where
-    -- part2 s = go newBoards
-    -- part2 s = sum $ map (f . isCycle) newBoards
-
-    -- part2 s = [b] <> newBoards
-    g (point, dir) = isLoop (S.union (bWalls b) (S.singleton point)) S.empty point dir
-
     b = pInput s
-    -- newBoards = (\p -> b{bWalls = S.insert p $ bWalls b})
+    (point, dir) = bGuard b
     movementPointsAndDir = moveGuardUntilLeave b
-    movementPoints = S.map fst movementPointsAndDir
-    height = bHeight b
-    width = bWidth b
-    inBounds (row, col) = inRange (0, height - 1) row && inRange (0, width - 1) col
-    possibleBlockPoints = S.toList $ S.filter inBounds $ S.map (\(point, dir) -> getMove dir point) movementPointsAndDir
-    -- possibleWallPoints = S.map fst movementPointsAndDir
-    newWallSets = S.toList $  S.map ((\(p, d) -> S.insert (getMove d p) (bWalls b))) movementPointsAndDir
-    newBoards = map (\p -> b{bWalls = S.insert p $ bWalls b}) possibleBlockPoints
-    f True = 1
-    f False = 0
-    go [] = 0
-    go lst = force (sum (parMap rdeepseq (f . isCycle) (take 100 lst))) + go (drop 100 lst)
+    newWallSets = S.toList $ S.map (\(p, d) -> S.insert (getMove d p) (bWalls b)) movementPointsAndDir
