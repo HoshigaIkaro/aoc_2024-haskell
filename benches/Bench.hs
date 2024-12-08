198bd16a7658abb5b9a3e8c1ab7f2da0de161044
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2024-12-08 00:43:16 -0500

    add benchmark for day 8

--- benches/Bench.hs
+++ benches/Bench.hs
@@ -7,6 +7,9 @@ import Days.D2 qualified as D2
 import Days.D3 qualified as D3
 import Days.D4 qualified as D4
 import Days.D5 qualified as D5
+import Days.D7 qualified as D7
+import Days.D8 qualified as D8
+
 
 main :: IO ()
 main = do
@@ -22,5 +25,10 @@ main = do
             "day 5"
             [ bench "part 1" $ whnf D5.part1 inputD5
             , bench "part 2" $ whnf D5.part2 inputD5
+            ]
+        , bgroup
+            "day 8"
+            [ bench "part 1" $ whnf D8.part1 inputD5
+            , bench "part 2" $ whnf D8.part2 inputD5
             ]
         ]
