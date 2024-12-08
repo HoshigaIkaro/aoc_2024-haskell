4b82ac05bdbc81728314bbb0f8348acbb4836a5f
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2024-12-07 23:57:38 -0500

    use log for int concat

--- src/Days/D7.hs
+++ src/Days/D7.hs
@@ -7,6 +7,7 @@ import Data.Maybe
 import Data.Text (Text)
 import Data.Text qualified as T
 import Data.Void (Void)
+import GHC.Float.RealFracMethods (ceilingDoubleInt, floorDoubleInt)
 import Text.Megaparsec
 import Text.Megaparsec.Char (space, string)
 import Text.Megaparsec.Char.Lexer qualified as L
@@ -15,16 +16,16 @@ type Parser = Parsec Void Text
 
 run :: IO ()
 run = do
-    input <- readFile "input/d7.txt"
-    print $ part1 input
-    print $ part2 input
+  input <- readFile "input/d7.txt"
+  print $ part1 input
+  print $ part2 input
 
 pLine :: Parser (Int, [Int])
 pLine = do
-    num <- L.decimal
-    void $ string ": "
-    values <- sepBy L.decimal space
-    pure (num, values)
+  num <- L.decimal
+  void $ string ": "
+  values <- sepBy L.decimal space
+  pure (num, values)
 
 pInput :: Text -> [(Int, [Int])]
 pInput = map (fromJust . parseMaybe pLine) . T.lines
@@ -35,11 +36,12 @@ isPossible ops current target (x : xs) = any (\op -> isPossible ops (current `op
 
 part1 :: String -> Int
 part1 = sum . map fst . filter (uncurry (isPossible operators 0)) . pInput . T.pack
-  where
-    operators = [(+), (*)]
+ where
+  operators = [(+), (*)]
 
 part2 :: String -> Int
 part2 = sum . map fst . filter (uncurry (isPossible operators 0)) . pInput . T.pack
-  where
-    f a b = read (show a <> show b)
-    operators = [(+), (*), f]
+ where
+  l = succ . floorDoubleInt . logBase 10 . fromIntegral
+  f a b = a * (10 ^ l b) + b
+  operators = [(+), (*), f]
