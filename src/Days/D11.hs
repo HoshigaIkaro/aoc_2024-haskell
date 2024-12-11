685a26a28f348c1834ce09f1f2290c953d849196
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2024-12-11 00:26:46 -0500

    use v2 for part 1

--- src/Days/D11.hs
+++ src/Days/D11.hs
@@ -1,6 +1,5 @@
 module Days.D11 (run, part1, part2) where
 
-import Data.Bits
 import Data.Map (Map)
 import Data.Map qualified as M
 import GHC.Float (double2Int, int2Double)
@@ -11,25 +10,12 @@ run = do
     print $ part1 input
     print $ part2 input
 
-evolve :: [Int] -> [Int]
-evolve [] = []
-evolve (x : xs)
-    | x == 0 = 1 : evolve xs
-    | even lb = x `div` (10 ^ mid) : (mod x (10 ^ mid)) : evolve xs
-    | otherwise = x * 2024 : evolve xs
-  where
-    lb = 1 + double2Int (logBase 10 (int2Double x))
-    mid = lb `div` 2
+type Count = Int
 
-evolveTimes :: Int -> [Int] -> [Int]
-evolveTimes 0 res = res
-evolveTimes n res = evolveTimes (n - 1) (evolve res)
+type Frequencies = Map Int Count
 
-part1 :: String -> Int
-part1 = length . evolveTimes 25 . map read . words
-
-evolveV2 :: Map Int Int -> Map Int Int
-evolveV2 = foldr f M.empty . M.toList
+evolve :: Frequencies -> Frequencies
+evolve = foldr f M.empty . M.toList
   where
     alterFunc key numX = M.alter (Just . maybe numX (numX +)) key
     f (x, numX) acc
@@ -42,14 +28,17 @@ evolveV2 = foldr f M.empty . M.toList
         lb = 1 + double2Int (logBase 10 (int2Double x))
         mid = lb `div` 2
 
-evolveTimesV2 :: Int -> Map Int Int -> Map Int Int
-evolveTimesV2 0 res = res
-evolveTimesV2 n res = evolveTimesV2 (n - 1) (evolveV2 res)
+evolveTimes :: Int -> Frequencies -> Frequencies
+evolveTimes 0 res = res
+evolveTimes n res = evolveTimes (n - 1) (evolve res)
 
-pMap :: String -> Map Int Int
-pMap = foldr f M.empty . map read . words
+pInput :: String -> Frequencies
+pInput = foldr (f . read) M.empty . words
   where
-    f n acc = M.alter (Just . maybe 1 succ) n acc
+    f = M.alter (Just . maybe 1 succ)
+
+part1 :: String -> Int
+part1 = sum . M.elems . evolveTimes 25 . pInput
 
 part2 :: String -> Int
-part2 = sum . M.elems . evolveTimesV2 75 . pMap
+part2 = sum . M.elems . evolveTimes 75 . pInput
