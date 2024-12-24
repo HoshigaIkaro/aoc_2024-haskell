4a02f4c3cd9bc32fcacd3435380e90853d165c60
Hoshiga Ikaro <hoshiga.ikaro@outlook.com>
2024-12-24 15:34:43 -0600

    comment unused functions

--- src/Days/D24.hs
+++ src/Days/D24.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE OverloadedStrings #-}
+{-# OPTIONS_GHC -Wno-unused-local-binds #-}
 
 module Days.D24 (run, part1, part2) where
 
@@ -108,103 +109,11 @@ part1 s = binToInt $ fst $ findValues valueMap outputMap 'z'
   where
     (valueMap, outputMap) = pInput s
 
-wiresAffecting :: Map Text (Gate, Text, Text) -> Text -> Set Text
-wiresAffecting outputMap target = go [outputMap M.! target] S.empty
-  where
-    go [] visited = visited
-    go ((_, a, b) : xs) visited = go xs (S.insert a $ S.insert b visited)
-
-outputAffectedBySwapping :: Map Text (Gate, Text, Text) -> Text -> Text -> Set Text
-outputAffectedBySwapping outputMap one two =
-    S.insert one $
-        S.insert two $
-            foldr (<>) mempty $
-                map (S.filter (`M.member` outputMap)) [wiresAffecting outputMap one, wiresAffecting outputMap two]
-
-type Pair = (Text, Text)
-
-allPairs :: Map Text (Gate, Text, Text) -> [(Text, Text)]
-allPairs = go . M.keys
-  where
-    go [] = []
-    go (x : xs) = [(x, y) | y <- xs] <> go xs
-
-allSwappableFourPairs :: Map Text (Gate, Text, Text) -> Set (Pair, Pair, Pair, Pair)
-allSwappableFourPairs outputMap = S.fromList $ nubByUnique $ go $ M.keys outputMap
-  where
-    func ((ax, ay), (bx, by), (cx, cy), (dx, dy)) = concat $ sort [sort [ax, ay], sort [bx, by], sort [cx, cy], sort [dx, dy]]
-    nubByUnique :: [(Pair, Pair, Pair, Pair)] -> [(Pair, Pair, Pair, Pair)]
-    nubByUnique = nubOrdOn func
-    go m = do
-        a <- m
-        b <- filter (/= a) m
-        c <- filter (`notElem` [a, b]) m
-        d <- filter (`notElem` [a, b, c]) m
-        e <- filter (`notElem` [a, b, c, d]) m
-        f <- filter (`notElem` [a, b, c, d, e]) m
-        g <- filter (`notElem` [a, b, c, d, e, f]) m
-        h <- filter (`notElem` [a, b, c, d, e, f, g]) m
-        pure ((a, b), (c, d), (e, f), (g, h))
-
--- fixForZ :: Map Text Int -> Map Text (Gate, Text, Text) -> Int -> Int -> Map Text (Gate, Text, Text)
--- fixForZ valueMap original zNum target = go $ allPairs original
---   where
---     go [] = original
---     go ((keyA, keyB) : xs) =
---         let oldValA = original M.! keyA
---             oldValB = original M.! keyB
---             swapped = M.insert keyA oldValB $ M.insert keyB oldValA original
---             (value, _) = findValue valueMap swapped $ zText zNum
---          in if value == target then swapped else go xs
-
-findSwaps :: Map Text Int -> Map Text (Gate, Text, Text) -> [Int] -> [Pair]
-findSwaps valueMap oOutputMap tBin = go oOutputMap 0 [] S.empty
-  where
-    go outputMap idx swappedPairs swapped
-        | length swappedPairs > 4 = []
-        | idx == length tBin =
-            if fst (findValues valueMap outputMap 'z') == tBin
-                then swappedPairs
-                else []
-        | fst (findValue valueMap outputMap (zText idx)) == tBin !! idx = go outputMap (idx + 1) swappedPairs swapped
-        | otherwise = do
-            pair@(keyA, keyB) <- filter (\pair@(a, b) -> a `S.notMember` swapped && b `S.notMember` swapped && (pair `notElem` swappedPairs)) $ allPairs outputMap
-            let newMap = swapPair outputMap pair
-                (value, _) = findValue valueMap newMap (zText idx)
-            guard (value == tBin !! idx)
-            go newMap (idx + 1) ((keyA, keyB) : swappedPairs) (S.insert keyA $ S.insert keyB swapped)
-
-zText :: Int -> Text
-zText zNum = T.pack ('z' : replicate (2 - length a) '0' <> a)
-  where
-    a = show zNum
-
 cText :: Int -> Char -> Text
 cText zNum c = T.pack (c : replicate (2 - length a) '0' <> a)
   where
     a = show zNum
 
-swapPair :: Map Text (Gate, Text, Text) -> (Text, Text) -> Map Text (Gate, Text, Text)
-swapPair outputMap (keyA, keyB) = newMap
-  where
-    oldValA = outputMap M.! keyA
-    oldValB = outputMap M.! keyB
-    newMap = M.insert keyA oldValB $ M.insert keyB oldValA outputMap
-
-gatesToBaseDepends :: Map Text (Gate, Text, Text) -> Map Text (Set Int) -> Text -> (Set Int, Map Text (Set Int))
-gatesToBaseDepends outputMap cache target
-    | "x" `T.isPrefixOf` target || "y" `T.isPrefixOf` target =
-        let v = read (drop 1 $ T.unpack target)
-            s = S.singleton v
-         in (s, M.insert target s cache)
-    | otherwise = case M.lookup target cache of
-        Just v -> (v, cache)
-        Nothing -> (combined, M.insert target combined wCache)
-  where
-    (_, keyA, keyB) = outputMap M.! target
-    (resA, vCache) = gatesToBaseDepends outputMap cache keyA
-    (resB, wCache) = gatesToBaseDepends outputMap vCache keyB
-    combined = resA <> resB
 
 toValueMapFor :: [Int] -> Char -> Map Text Int
 toValueMapFor lst c = go $ zip [0 ..] lst
@@ -229,37 +138,6 @@ testMachineFor machine numOutputBits zNum = go 50 (mkStdGen 1)
             valueMap = toValueMapFor (padded xBin) 'x' <> toValueMapFor (padded yBin) 'y'
          in take (zNum + 1) (fst (findValues valueMap machine 'z')) == selected && go (n - 1) gen3
 
-fixMachine :: Map Text (Gate, Text, Text) -> Map Text (Gate, Text, Text)
-fixMachine originalMachine = go 0 originalMachine S.empty
-  where
-    numOutputBits = length $ filter ("z" `T.isPrefixOf`) $ M.keys originalMachine
-    go zNum machine visited
-        | zNum == numOutputBits + 1 = machine
-        | testMachineFor machine numOutputBits zNum = go (zNum + 1) machine visited
-        | otherwise = go (zNum + 1) fixedMachine (S.singleton (cText zNum 'z') <> visited <> wiresAffecting fixedMachine (cText zNum 'z'))
-      where
-        filteredMachine = M.filterWithKey (\key _ -> key `S.notMember` visited) machine
-        fixedMachine = findWorkingSwappedMachine machine (allPairs filteredMachine) zNum
-    findWorkingSwappedMachine machine [] _ = machine
-    findWorkingSwappedMachine machine (pair : rest) zNum
-        | testMachineFor swapped numOutputBits zNum = swapped
-        | otherwise = findWorkingSwappedMachine machine rest zNum
-      where
-        swapped = swapPair machine pair
-
-fixMachineAt :: Map Text (Gate, Text, Text) -> Int -> Map Text (Gate, Text, Text)
-fixMachineAt originalMachine zNum
-    | testMachineFor originalMachine numOutputBits zNum = originalMachine
-    | otherwise = findWorkingSwappedMachine originalMachine (allPairs originalMachine)
-  where
-    numOutputBits = length $ filter ("z" `T.isPrefixOf`) $ M.keys originalMachine
-    findWorkingSwappedMachine machine [] = machine
-    findWorkingSwappedMachine machine (pair : rest)
-        | testMachineFor swapped numOutputBits zNum = swapped
-        | otherwise = findWorkingSwappedMachine machine rest
-      where
-        swapped = swapPair machine pair
-
 andColor :: Attribute
 andColor = Color [WC (toColor Orange) Nothing]
 
@@ -268,17 +146,6 @@ orColor = Color [WC (toColor LightBlue) Nothing]
 
 xorColor :: Attribute
 xorColor = Color [WC (toColor Red) Nothing]
-
-createGraph :: Map Text (Gate, Text, Text) -> DotGraph Text
-createGraph = toCanonical . go . M.toList
-  where
-    go [] = emptyGraph
-    go ((output, (gate, inputA, inputB)) : rest)
-        | gate == AND = addDotNode (DotNode output [andColor]) $ edgesC $ go rest
-        | gate == OR = addDotNode (DotNode output [orColor]) $ edgesC $ go rest
-        | otherwise = addDotNode (DotNode output [xorColor]) $ edgesC $ go rest
-      where
-        edgesC = addDotEdge (DotEdge inputA output []) . addDotEdge (DotEdge inputB output [])
 
 nodes :: Map Text (Gate, Text, Text) -> [DotNode Text]
 nodes mapping = go (M.keys mapping) S.empty
@@ -302,21 +169,136 @@ edges = go . M.toList
     go [] = []
     go ((output, (_, inputA, inputB)) : rest) = DotEdge inputA output [] : DotEdge inputB output [] : go rest
 
+
+-- | part 2 was done by manually inspecting output/d24.png and using the test machine function
 -- part2 :: String -> [Int]
 -- part2 s = createGraph outputMap
 -- part2 s = runGraphvizCommand Dot (createGraph outputMap) Png "output/d24.png"
 -- part2 s = runGraphvizCommand Dot (mkGraph (nodes outputMap) (edges outputMap)) Png "output/d24.png"
 -- part2 s = runGraphvizCommand Dot dg Png "output/d24.png"
-part2 s = pure $ testMachineFor outputMap 46 45
+part2 :: String -> IO String
+part2 s = print (testMachineFor outputMap 46 45) >> pure ""
   where
     dg = toCanonical $ mkGraph (nodes outputMap) (edges outputMap)
     st = graphStatements dg
     att = attrStmts st
     ndg = dg{graphStatements = st{attrStmts = [GraphAttrs [RankDir FromTop]]}}
-    (valueMap, outputMap) = pInput s
+    (_valueMap, outputMap) = pInput s
     -- (xBinValue, aMap) = findValues valueMap outputMap 'x'
     -- (yBinValue, bMap) = findValues aMap outputMap 'y'
     -- (zBinValue, cMap) = findValues bMap outputMap 'z'
     -- target = intToBin (binToInt xBinValue + binToInt yBinValue)
     -- targetBin = target <> replicate (length zBinValue - length target) 0
     -- cValueMap = M.insert "y01" 1 $ M.insert "y00" 1 $ M.insert "x00" 1 $ M.map (const 0) valueMap
+
+
+
+-- swapPair :: Map Text (Gate, Text, Text) -> (Text, Text) -> Map Text (Gate, Text, Text)
+-- swapPair outputMap (keyA, keyB) = newMap
+--   where
+--     oldValA = outputMap M.! keyA
+--     oldValB = outputMap M.! keyB
+--     newMap = M.insert keyA oldValB $ M.insert keyB oldValA outputMap
+
+-- gatesToBaseDepends :: Map Text (Gate, Text, Text) -> Map Text (Set Int) -> Text -> (Set Int, Map Text (Set Int))
+-- gatesToBaseDepends outputMap cache target
+--     | "x" `T.isPrefixOf` target || "y" `T.isPrefixOf` target =
+--         let v = read (drop 1 $ T.unpack target)
+--             s = S.singleton v
+--          in (s, M.insert target s cache)
+--     | otherwise = case M.lookup target cache of
+--         Just v -> (v, cache)
+--         Nothing -> (combined, M.insert target combined wCache)
+--   where
+--     (_, keyA, keyB) = outputMap M.! target
+--     (resA, vCache) = gatesToBaseDepends outputMap cache keyA
+--     (resB, wCache) = gatesToBaseDepends outputMap vCache keyB
+--     combined = resA <> resB
+
+
+-- fixMachine :: Map Text (Gate, Text, Text) -> Map Text (Gate, Text, Text)
+-- fixMachine originalMachine = go 0 originalMachine S.empty
+--   where
+--     numOutputBits = length $ filter ("z" `T.isPrefixOf`) $ M.keys originalMachine
+--     go zNum machine visited
+--         | zNum == numOutputBits + 1 = machine
+--         | testMachineFor machine numOutputBits zNum = go (zNum + 1) machine visited
+--         | otherwise = go (zNum + 1) fixedMachine (S.singleton (cText zNum 'z') <> visited <> wiresAffecting fixedMachine (cText zNum 'z'))
+--       where
+--         filteredMachine = M.filterWithKey (\key _ -> key `S.notMember` visited) machine
+--         fixedMachine = findWorkingSwappedMachine machine (allPairs filteredMachine) zNum
+--     findWorkingSwappedMachine machine [] _ = machine
+--     findWorkingSwappedMachine machine (pair : rest) zNum
+--         | testMachineFor swapped numOutputBits zNum = swapped
+--         | otherwise = findWorkingSwappedMachine machine rest zNum
+--       where
+--         swapped = swapPair machine pair
+
+-- fixMachineAt :: Map Text (Gate, Text, Text) -> Int -> Map Text (Gate, Text, Text)
+-- fixMachineAt originalMachine zNum
+--     | testMachineFor originalMachine numOutputBits zNum = originalMachine
+--     | otherwise = findWorkingSwappedMachine originalMachine (allPairs originalMachine)
+--   where
+--     numOutputBits = length $ filter ("z" `T.isPrefixOf`) $ M.keys originalMachine
+--     findWorkingSwappedMachine machine [] = machine
+--     findWorkingSwappedMachine machine (pair : rest)
+--         | testMachineFor swapped numOutputBits zNum = swapped
+--         | otherwise = findWorkingSwappedMachine machine rest
+--       where
+--         swapped = swapPair machine pair
+
+
+-- wiresAffecting :: Map Text (Gate, Text, Text) -> Text -> Set Text
+-- wiresAffecting outputMap target = go [outputMap M.! target] S.empty
+--   where
+--     go [] visited = visited
+--     go ((_, a, b) : xs) visited = go xs (S.insert a $ S.insert b visited)
+
+-- outputAffectedBySwapping :: Map Text (Gate, Text, Text) -> Text -> Text -> Set Text
+-- outputAffectedBySwapping outputMap one two =
+--     S.insert one $
+--         S.insert two $
+--             foldr (<>) mempty $
+--                 map (S.filter (`M.member` outputMap)) [wiresAffecting outputMap one, wiresAffecting outputMap two]
+
+-- type Pair = (Text, Text)
+
+-- allPairs :: Map Text (Gate, Text, Text) -> [(Text, Text)]
+-- allPairs = go . M.keys
+--   where
+--     go [] = []
+--     go (x : xs) = [(x, y) | y <- xs] <> go xs
+
+-- allSwappableFourPairs :: Map Text (Gate, Text, Text) -> Set (Pair, Pair, Pair, Pair)
+-- allSwappableFourPairs outputMap = S.fromList $ nubByUnique $ go $ M.keys outputMap
+--   where
+--     func ((ax, ay), (bx, by), (cx, cy), (dx, dy)) = concat $ sort [sort [ax, ay], sort [bx, by], sort [cx, cy], sort [dx, dy]]
+--     nubByUnique :: [(Pair, Pair, Pair, Pair)] -> [(Pair, Pair, Pair, Pair)]
+--     nubByUnique = nubOrdOn func
+--     go m = do
+--         a <- m
+--         b <- filter (/= a) m
+--         c <- filter (`notElem` [a, b]) m
+--         d <- filter (`notElem` [a, b, c]) m
+--         e <- filter (`notElem` [a, b, c, d]) m
+--         f <- filter (`notElem` [a, b, c, d, e]) m
+--         g <- filter (`notElem` [a, b, c, d, e, f]) m
+--         h <- filter (`notElem` [a, b, c, d, e, f, g]) m
+--         pure ((a, b), (c, d), (e, f), (g, h))
+
+-- findSwaps :: Map Text Int -> Map Text (Gate, Text, Text) -> [Int] -> [Pair]
+-- findSwaps valueMap oOutputMap tBin = go oOutputMap 0 [] S.empty
+--   where
+--     go outputMap idx swappedPairs swapped
+--         | length swappedPairs > 4 = []
+--         | idx == length tBin =
+--             if fst (findValues valueMap outputMap 'z') == tBin
+--                 then swappedPairs
+--                 else []
+--         | fst (findValue valueMap outputMap (cText idx 'z')) == tBin !! idx = go outputMap (idx + 1) swappedPairs swapped
+--         | otherwise = do
+--             pair@(keyA, keyB) <- filter (\pair@(a, b) -> a `S.notMember` swapped && b `S.notMember` swapped && (pair `notElem` swappedPairs)) $ allPairs outputMap
+--             let newMap = swapPair outputMap pair
+--                 (value, _) = findValue valueMap newMap (cText idx 'z')
+--             guard (value == tBin !! idx)
+--             go newMap (idx + 1) ((keyA, keyB) : swappedPairs) (S.insert keyA $ S.insert keyB swapped)
